8bafae894a9e3d73c7dee8dd662bc1d173ec2c5c
Slazaa <afitero@gmail.com>
2022-10-04 16:58:48 +0200

    Update parser

--- src/parser.rs
+++ src/parser.rs
@@ -25,10 +25,14 @@ where
 		}
 	}
 
-	pub fn eval_pattern(&mut self, lexer_stream: &mut LexerStream, pattern_name: &str, tokens: &mut Vec<Token>) -> Result<N, (String, Position)> {
+	pub fn eval_pattern(&mut self, lexer_stream: &mut LexerStream, pattern: &Pattern<N>) -> Result<N, (String, Position)> {
+		todo!();
+	}
+
+	pub fn eval_pattern_by_name(&mut self, lexer_stream: &mut LexerStream, pattern_name: &str, tokens: &mut Vec<Token>) -> Result<N, (String, Position)> {
 		let patterns: Vec<Pattern<N>> = self.patterns.iter().filter(|x| x.name() == pattern_name).map(|x| x.clone()).collect();
 		let mut added_tokens = 0;
-		//let mut expected_elems = Vec::new();
+		let mut expected_elems = Vec::new();
 
 		'pattern_loop: for pattern in patterns {
 			let elems = pattern.elems();
@@ -58,13 +62,15 @@ where
 				}
 			}
 
+			expected_elems.push(elems[0].clone());
+
 			let elems_tokens = &tokens.clone()[tokens.len()-elems.len()..];
 			let mut nodes = Vec::new();
 
 			for (elem, token) in elems.iter().zip(elems_tokens.iter()) {
 				// Check if elem is a pattern
 				if self.patterns.iter().any(|x| x.name() == elem) {
-					nodes.push(self.eval_pattern(lexer_stream, elem, tokens)?);
+					nodes.push(self.eval_pattern_by_name(lexer_stream, elem, tokens)?);
 				}
 				// Check if elem is a token
 				else if self.token_names.contains(elem) {
@@ -77,10 +83,11 @@ where
 			}
 
 			if elems.len() == nodes.len() {
+				// TODO: - Try other patterns
 				return Ok(pattern.func()(&nodes[..]));
 			}
 		}
-	/*
+	
 		let mut expected_str = String::new();
 
 		for (i, exp) in expected_elems.iter().enumerate() {
@@ -94,12 +101,10 @@ where
 		}
 
 		Err((format!("Failed creating '{}' node, expected {}", pattern_name, expected_str), self.pos))
-	*/
-		todo!();
 	}
 	
 	pub fn parse(&mut self, mut lexer_stream: LexerStream) -> Result<N, (String, Position)> {
-		let res = self.eval_pattern(&mut lexer_stream, "program", &mut Vec::new());
+		let res = self.eval_pattern_by_name(&mut lexer_stream, "program", &mut Vec::new());
 
 		if lexer_stream.next().is_some() {
 			return Err(("Tokens remaining".to_owned(), self.pos));
